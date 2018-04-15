e62a6858c16ed329bdf699071d16ad4b5254a8c2
s <s@s-arch>
2018-04-15 13:50:43 -0300

    Alterações pequenas

--- src/teste/ListaTest.java
+++ src/teste/ListaTest.java
@@ -1,15 +1,11 @@
 package teste;
 
 import static org.junit.Assert.*;
-
-import java.util.Arrays;
-
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.rules.TestName;
 
 import trabalho.IGenerico;
@@ -46,34 +42,30 @@ public class ListaTest {
 		@Test()
 		public void test1() {
 			try {
-				System.out.println("Testando se é lançada uma exceção ao tentar excluir "
-						+ "um objeto da lista quando a mesma está vazia");
-				this.lista.excluir(6);
+				lista.excluir(6);
 				fail("Uma exceção deveria ser lançada");
 			}catch(Exception e) {
-				System.out.println("A exceção que deve ser lançada é 'Lista vazia'");
 				assertEquals("Lista vazia", e.getMessage());
-				System.out.println("O teste passou");
 			}
 		}
 		
 		@Test
 		public void test2() throws Exception {
-			this.lista.insereOrdenado(aluno5);
-			this.lista.insereOrdenado(aluno2);
-			this.lista.insereOrdenado(aluno3);
-			this.lista.insereOrdenado(aluno1);
-			this.lista.insereOrdenado(aluno4);
+			lista.insereOrdenado(aluno5);
+			lista.insereOrdenado(aluno2);
+			lista.insereOrdenado(aluno3);
+			lista.insereOrdenado(aluno1);
+			lista.insereOrdenado(aluno4);
 			IGenerico[] experado = new IGenerico[]{aluno4, aluno2, aluno5, aluno1, aluno3};
-			Assert.assertArrayEquals(experado, this.lista.listaTodos());
+			Assert.assertArrayEquals(experado, lista.listaTodos());
 			//assertTrue(Arrays.equals(experado, lista.listaTodos()));
 		}
 			
 		@Test
 		public void test3() {
 			try {
-				this.lista.excluir(18);
-				this.lista.buscar(18);
+				lista.excluir(18);
+				lista.buscar(18);
 				fail("Uma exceção deveria ser lançada");
 			}catch(Exception e) {
 				assertEquals("O objeto com o id 18 não existe na lista", e.getMessage());
@@ -84,14 +76,14 @@ public class ListaTest {
 		
 		@Test
 		public void test4() throws Exception {
-			Assert.assertSame(aluno1, this.lista.buscar(39));
+			Assert.assertSame(aluno1, lista.buscar(39));
 			
 		}
 		
 		@Test
 		public void test5() {
 			try {
-				this.lista.insereOrdenado(alunoRepetido);
+				lista.insereOrdenado(alunoRepetido);
 				fail("Uma exceção deve ser lançada");
 			}catch(Exception e) {
 				assertEquals("Já existe um objeto com a id 45 na lista", e.getMessage());
